3481387f8ec4ec8488c7295064a439d20527abbd
Niko Savas <nikosavas@hotmail.com>
2015-02-25 10:36:32 -0500

    Created libGDX clean project

---
+++ proj/desktop/src/com/groupfive/connectfour/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.groupfive.connectfour.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.groupfive.connectfour.ConnectFour;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ConnectFour(), config);
+	}
+}
---
+++ proj/html/src/com/groupfive/connectfour/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.groupfive.connectfour.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.groupfive.connectfour.ConnectFour;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new ConnectFour();
+        }
+}
