e7213536f1f5ba943ad8e4cc6b660ee0f46dc0bb
JemarJones <jemarkjones@gmail.com>
2015-03-11 20:29:37 -0400

    Removed confusing folder

--- gamelogic/ErrorCode.java
+++
@@ -1,6 +0,0 @@
-package gamelogic;
-
-//An enum for the isValid() method
-public enum ErrorCode{
-	DEFIESGRAVITY, BADRATIO, NOWINNINGALLOWED
-}
--- gamelogic/TokenState.java
+++
@@ -1,6 +0,0 @@
-package gamelogic;
-
-//An enum representing the states of token can have
-public enum TokenState{
-	RED, BLUE, EMPTY
-}
