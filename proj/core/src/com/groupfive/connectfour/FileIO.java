bab7d0205d879208e1728b626443e5c3fe8e0d61
tylerpost <tyler.post200@gmail.com>
2015-03-12 20:28:26 -0400

    Update FileIO.java
    finished save

--- proj/core/src/com/groupfive/connectfour/FileIO.java
+++ proj/core/src/com/groupfive/connectfour/FileIO.java
@@ -8,45 +8,102 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class FileIO{
+	
+	
   
-
-  //should actually be void not sure what is actually passed
   //Saves the current gamestate into a textfile
-  public static void save(String fileName, Board currBoard)throws IOException {
+  public static void save(Board currBoard, String pTurn)throws IOException {
 
-  
-    	Writer wr = null;
-	try {
-		File f = new File(fileName);
-		wr = new BufferedWriter(new FileWriter(f));
-  
-        	for (int i = 0; i < 8; i++){
-          		for (int j = 0; j < 7; j++){
-            			Token temp = currBoard.getToken(i,j);
-            
-				if (temp.getState() == TokenState.RED){
-					wr.write("R");
+	  //Read contents of file into arraylist data
+	  BufferedReader br = null;
+	  try{
+			br = new BufferedReader(new FileReader("save.txt"));
+			while((line = br.readLine()) != null){
+				String[] temp = line.split(",");
+				data.add(temp);
+			}
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		finally{
+			if (br != null){
+				try{
+					br.close();
 				}
-				else if(temp.getState() == TokenState.BLUE){
-					wr.write("B");
+				catch (IOException e){
+					e.printStackTrace();
 				}
-				else{
-					wr.write("-");
-            			}
-          		}
-        	}
-      	}
-      	finally {
-		if (wr != null){
-			wr.close();
+			}
 		}
-    
-      	}	
+	
+	  	//saves the current date/time
+		Date myDate = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy:HH-mm-ss");
+		String date = sdf.format(myDate);
+		
+		
+		//Construct a string of the format RB- based on the state of the gameBoard
+		String gameTokens = "";
+		
+    	for (int i = 0; i < 8; i++){
+      		for (int j = 0; j < 7; j++){
+        			Token temp = currBoard.getToken(i,j);
+        
+			if (temp.getState() == TokenState.RED){
+				gameTokens = gameTokens.append("R");
+			}
+			else if(temp.getState() == TokenState.BLUE){
+				gameTokens = gameTokens.append("B");
+			}
+			else{
+				gameTokens = gameTokens.append("-");
+        		}
+      		}
+    	}
+		
+    	
+    	//add the current state to the list of saved games
+    	String[] temp1 = new String[]{date, gameTokens, pTurn};
+    	data.add([temp1]);
+		
+		//Write the list of saved games to save.txt 
+    	Writer wr = null;
+    	try {
+    		File f = new File("save.txt");
+    		wr = new BufferedWriter(new FileWriter(f));
+    	
+    		for(int k = 0; k < data.size(); k++){
+    			for(int l = 0; l<3; l++){
+    				wr.write(data.get(k)[l] + "@");
+    			}
+    			wr.write("\n");
+    		}
+
+      	}catch(FileNotFoundException e){
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		finally{
+			if (wr != null){
+				try{
+					wr.close();
+				}
+				catch (IOException e){
+					e.printStackTrace();
+				}
+			}
+		}
   }
-  
-  //Loads the current gameState from a textFile
+
+ //Loads the current gameState from a textFile
   public static void load(String fileName, GameController currGame) throws IOException{
   	BufferedReader input = new BufferedReader(new FileReader(fileName));
   	
