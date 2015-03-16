8a7f3c88e66d7fffbf2d651bba350ad52545b709
joenot443 <joe.crozier@live.ca>
2015-03-16 13:41:27 -0400

    Added loading GUI
    Pressing the load button on Menu will generate load buttons. Pressing
    these buttons will call loadGame(int i), where i is the index of the
    game you wish to load.

--- proj/core/src/com/groupfive/connectfour/MMenu.java
+++ proj/core/src/com/groupfive/connectfour/MMenu.java
@@ -2,18 +2,24 @@ package com.groupfive.connectfour;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.ui.List.ListStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.Array;
 
 
 public class MMenu implements Screen{
@@ -26,8 +32,11 @@ public class MMenu implements Screen{
 	 private Stage stage;
 	 private TextureAtlas atlas;
 	 private Skin skin;
-	 private Table table;
-	 private TextButton buttonExit, buttonPlay, buttonCreate;
+	 private Table table, loadTable;
+	 private TextButton buttonExit, buttonPlay, buttonCreate, buttonLoad;
+	 private List loadList;
+	 
+	  
 	 
 	
 	
@@ -71,7 +80,7 @@ public class MMenu implements Screen{
 		 table = new Table(skin);
 		 table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		 
-		 //Create and implent the PLAY button
+		 //Create and implement the PLAY button
 		 buttonPlay = new TextButton("Play", skin);
 		 buttonPlay.pad(20);
 		 buttonPlay.addListener(new ClickListener(){
@@ -79,6 +88,17 @@ public class MMenu implements Screen{
 		 @Override
 		 public void clicked(InputEvent event, float x, float y) {
 		 game.setScreen(new GameScreen(game,false));
+		 }
+		 });
+		 
+		//Create and implement the load game button
+		 
+		 buttonLoad = new TextButton("Load Game", skin);
+		 buttonLoad.pad(20);
+		 buttonLoad.addListener(new ClickListener(){
+		 @Override
+		 public void clicked(InputEvent event, float x, float y){
+			 displayLoader(50);
 		 }
 		 });
 		 
@@ -103,19 +123,75 @@ public class MMenu implements Screen{
 		 });
 		 
 
-		 table.add(buttonPlay);
-		 table.getCell(buttonPlay).spaceBottom(20);
+		 table.add(buttonPlay).height(80);
+		 table.getCell(buttonPlay).spaceBottom(10);
 		 table.row();
-		 table.add(buttonCreate);
-		 table.getCell(buttonCreate).spaceBottom(20);
+		 table.add(buttonCreate).height(80);;
+		 table.getCell(buttonCreate).spaceBottom(10);
 		 table.row();
-		 table.add(buttonExit);
-		 table.getCell(buttonExit).spaceBottom(20);
+		 table.add(buttonLoad).height(80);
+		 table.getCell(buttonLoad).spaceBottom(10);
+		 table.row();
+		 table.add(buttonExit).height(80);
+		 table.getCell(buttonExit).spaceBottom(10);
 		 table.bottom();
 		 stage.addActor(table);
 		 
 	}
+	//Called when buttonLoad is pressed
+	
+	public void displayLoader(int count) {
+		//Hide our table of buttons
+		table.setVisible(false);
+		
+		//Start drawing a new table of buttons
+		
+		 loadTable = new Table(skin);
+		 loadTable.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		 int r = 0;
+		 //Create a new button for every save slot
+		for (int i=0; i < count; i++) {
+			//Increment r
+			 r++;
+			 TextButton buttonSelectLoad = new TextButton(String.format("Save #%d", i), skin);
+			 buttonSelectLoad.pad(20);
+			 //We must redefine i in order for it to be used in the following method
+			 
+			 final int j = i;
+			 
+			 //Create a method which is called when the button is clicked
+			 buttonSelectLoad.addListener(new ClickListener(){
+			 @Override
+			 public void clicked(InputEvent event, float x, float y) {
+				 loadGame(j);
+			 }
+			 });
+			 
+			 //Add our created button to the table
+			 
+			 loadTable.add(buttonSelectLoad).height(80);
+			 loadTable.getCell(buttonSelectLoad).spaceBottom(10);
+			 
+			 //On every second value of r, start a new row.
+			 
+			 if (r == 2) { 
+				 loadTable.row();
+				 r = 0;
+			 }
 
+		}
+		loadTable.bottom();
+		stage.addActor(loadTable);
+		
+		
+	}
+	
+	//Loads the game at the selected index
+	
+	public void loadGame(int index) {
+		System.out.println(index);
+	}
+	
 	@Override
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
@@ -149,7 +225,8 @@ public class MMenu implements Screen{
 		 
 		
 	}
-
+	
+	
 	
 	
 }
