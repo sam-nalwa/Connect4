debf2d38815187a0806e3d51973681850d464d71
Sam Nalwa <me@samrajnalwa.com>
2015-03-17 11:54:31 -0400

    Adding save feature

--- proj/core/src/com/groupfive/connectfour/FileIO.java
+++ proj/core/src/com/groupfive/connectfour/FileIO.java
@@ -7,6 +7,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.io.Writer;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -21,45 +23,54 @@ public class FileIO{
 		
 	 ArrayList<String[]> data = new ArrayList<String[]>();
 	  //Read contents of file into arraylist data
-	  BufferedReader br = null;
+	  BufferedReader br = new BufferedReader(new FileReader("assets/save.txt"));
+	  PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("assets/save.txt")));
+	 // PrintStream output = new PrintStream(new File("save.txt"));
+	  String line="";
+	  int count=0;
 	  try{
-			br = new BufferedReader(new FileReader("save.txt"));
-			while((line = br.readLine()) != null){
-				String[] temp = line.split(",");
-				data.add(temp);
-			}
+		  while((/*line = */br.readLine()) != null){
+				//String[] temp = line.split(",");
+				//data.add(temp);
+			  count++;
+		  }
+			//Construct a string of the format RB- based on the state of the gameBoard
+		  String gameTokens = "";
+
+		  for (int i = 0; i < 8; i++){
+			  for (int j = 0; j < 7; j++){
+				  Token temp = currBoard.getToken(i,j);
+
+				  if (temp.getState() == TokenState.RED){
+					  gameTokens +="R";
+				  }
+				  else if(temp.getState() == TokenState.BLUE){
+					  gameTokens += "B";
+				  }
+				  else{
+					  gameTokens += "-";
+				  }
+			  }
+		  }
+		  
+		  //saves the current date/time
+		  Date myDate = new Date();
+		  SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy:HH-mm-ss");
+		  String date = sdf.format(myDate);
+		  out.println(date+"@"+gameTokens+"@"+pTurn);
+		  
 		}catch(FileNotFoundException e){
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} 
+	  
 		br.close();
-	
-	  	//saves the current date/time
-		Date myDate = new Date();
-		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy:HH-mm-ss");
-		String date = sdf.format(myDate);
+		out.close();
 		
-		
-		//Construct a string of the format RB- based on the state of the gameBoard
-		String gameTokens = "";
-		
-    	for (int i = 0; i < 8; i++){
-      		for (int j = 0; j < 7; j++){
-        			Token temp = currBoard.getToken(i,j);
-        
-			if (temp.getState() == TokenState.RED){
-				gameTokens = gameTokens.append("R");
-			}
-			else if(temp.getState() == TokenState.BLUE){
-				gameTokens = gameTokens.append("B");
-			}
-			else{
-				gameTokens = gameTokens.append("-");
-        		}
-      		}
-    	}
-		
+
+}
+/*		
     	
     	//add the current state to the list of saved games
     	String[] temp1 = new String[]{date, gameTokens, pTurn};
@@ -85,6 +96,7 @@ public class FileIO{
 		} 
 		wr.close();
   }
+ */
 
  //Loads the gameState specified at index loadGame
   public static void load(Integer loadGame) throws IOException{
@@ -96,14 +108,17 @@ public class FileIO{
 	 
 	  //Read contents of file to get token Locations and player turn
 	  BufferedReader br = null;
+	 /*
 	  try{
 			br = new BufferedReader(new FileReader("save.txt"));
 			while((line = br.readLine())  != null ){
+				
 				if(lineCount = loadGame){
 					String[] temp = line.split("@");
 					tokenStates = temp[1];
 					playerTurn = temp[2];
 				}
+				
 			}
 		}catch(FileNotFoundException e){
 			e.printStackTrace();
@@ -111,8 +126,9 @@ public class FileIO{
 			e.printStackTrace();
 		} 
 		br.close();
-  	
-	  //
+		*/
+		
+	 
 	  for(int i = 0; i<tokenStates.length(); i++){
 		  char currToken = tokenStates.charAt(i);
 		  if (currToken == 'R'){
